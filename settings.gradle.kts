d4a70deee826100891968a3d238e50fcd72b0b3d
NikkyAI <root@nikky.moe>
2018-11-03 20:12:35 +0100

    cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,9 @@
 pluginManagement {
     repositories {
-//        mavenCentral()
         maven(url = "http://maven.modmuss50.me"){
             name = "Fabric"
         }
         gradlePluginPortal()
     }
-//    resolutionStrategy {
-//        eachPlugin {
-//            if(requested.id.id == "net.fabricmc") {
-//                useModule("net.fabricmc:fabric-loom:${requested.version}")
-//            }
-//        }
-//    }
 }
 rootProject.name = "fabric-language-kotlin"
