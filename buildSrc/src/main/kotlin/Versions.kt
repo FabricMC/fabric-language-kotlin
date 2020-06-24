0ee084fd53fed2cf7272d4eeaa21cdca7a2df7d0
james58899 <james59988@gmail.com>
2020-06-24 14:34:45 +0800

    Kotlin 1.3.72 coroutines 1.3.7 Minecraft 1.16

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ object Jetbrains {
     const val annotations = "org.jetbrains:annotations:$annotationsVersion"
 
     object Kotlin {
-        const val version = "1.3.71"
+        const val version = "1.3.72"
         const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
         const val stdLibJkd7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
         const val stdLibJkd8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
@@ -11,7 +11,7 @@ object Jetbrains {
     }
 
     object KotlinX {
-        private const val coroutinesVersion = "1.3.5"
+        private const val coroutinesVersion = "1.3.7"
         const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
         const val coroutinesJdk8 = "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion"
     }
@@ -19,24 +19,24 @@ object Jetbrains {
 
 object Fabric {
     object Loader {
-        const val version = "0.7.3+build.176" // https://maven.fabricmc.net/net/fabricmc/fabric-loader/
+        const val version = "0.8.8+build.202" // https://maven.fabricmc.net/net/fabricmc/fabric-loader/
     }
 
     object API {
-        const val version = "0.5.1+build.294-1.15"
+        const val version = "0.13.1+build.370-1.16"
     }
 
     object Loom {
-        const val version = "0.2.7-SNAPSHOT"
+        const val version = "0.4-SNAPSHOT"
     }
 
     object YarnMappings {
-        const val version = "${Minecraft.version}+build.9"
+        const val version = "${Minecraft.version}+build.1"
     }
 }
 
 object Minecraft {
-    const val version = "1.15.2"
+    const val version = "1.16"
 }
 
 object CurseGradle{
