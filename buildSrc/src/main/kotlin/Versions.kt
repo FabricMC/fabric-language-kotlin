e727a4f8ec6fbd061f33cc93b9241d8d171e2063
Jakob K <52456572+jakobkmar@users.noreply.github.com>
2021-09-30 16:01:15 +0200

    Update compiler and standard libraries to 1.5.31 (#54)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Jetbrains {
 
     object Kotlin {
-        const val version = "1.5.30"
+        const val version = "1.5.31"
         const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
         const val stdLibJkd7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
         const val stdLibJkd8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
@@ -10,13 +10,13 @@ object Jetbrains {
 
     object KotlinX {
         object Coroutines {
-            const val version = "1.5.1"
+            const val version = "1.5.2"
             const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
             const val coreJvm = "org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$version"
             const val jdk8 = "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$version"
         }
         object Serialization {
-            const val version = "1.2.2"
+            const val version = "1.3.0"
             const val coreJvm = "org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$version"
             const val jsonJvm = "org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$version"
         }
@@ -25,7 +25,7 @@ object Jetbrains {
 
 object Fabric {
     object Loader {
-        const val version = "0.11.2" // https://maven.fabricmc.net/net/fabricmc/fabric-loader/
+        const val version = "0.11.7" // https://maven.fabricmc.net/net/fabricmc/fabric-loader/
     }
 
     object Loom {
