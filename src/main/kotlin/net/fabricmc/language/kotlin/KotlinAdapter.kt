efd0ff48e495b7389c3a463e264b91f118ef3a18
modmuss <modmuss50@gmail.com>
2023-11-01 15:17:05 +0000

    Fix #120 (#121)

--- src/main/kotlin/net/fabricmc/language/kotlin/KotlinAdapter.kt
+++ src/main/kotlin/net/fabricmc/language/kotlin/KotlinAdapter.kt
@@ -20,15 +20,14 @@ import net.fabricmc.loader.api.LanguageAdapter
 import net.fabricmc.loader.api.LanguageAdapterException
 import net.fabricmc.loader.api.ModContainer
 import net.fabricmc.loader.launch.common.FabricLauncherBase
-import net.fabricmc.loader.util.DefaultLanguageAdapter
 import java.lang.reflect.Proxy
 import kotlin.reflect.full.*
 import kotlin.reflect.jvm.jvmErasure
-import kotlin.reflect.jvm.reflect
-import kotlin.system.exitProcess
 
 open class KotlinAdapter : LanguageAdapter {
     override fun <T : Any> create(mod: ModContainer, value: String, type: Class<T>): T {
+        // TODO dont use none API fabric loader
+        val targetClassLoader = FabricLauncherBase.getLauncher().targetClassLoader
         val methodSplit = value.split("::").dropLastWhile { it.isEmpty() }.toTypedArray()
         val methodSplitSize = methodSplit.size
         if (methodSplitSize >= 3) {
@@ -36,7 +35,7 @@ open class KotlinAdapter : LanguageAdapter {
         }
 
         val c: Class<Any> = try {
-            Class.forName(methodSplit[0], true, FabricLauncherBase.getLauncher().targetClassLoader) as Class<Any>
+            Class.forName(methodSplit[0], true, targetClassLoader) as Class<Any>
         } catch (e: ClassNotFoundException) {
             throw LanguageAdapterException(e)
         }
@@ -58,56 +57,54 @@ open class KotlinAdapter : LanguageAdapter {
                 }
             }
             2 -> {
-                try {
-                    val instance = k.objectInstance ?: run {
-                        throw LanguageAdapterException("$k is not a object")
-                    }
-                    val methodList = instance::class.memberFunctions.filter { m ->
-                        m.name == methodSplit[1]
-                    }
-
-                    k.declaredMemberProperties.find {
-                        it.name == methodSplit[1]
-                    }?.let { field ->
-                        try {
-                            val fType = field.returnType
-
-                            if (methodList.isNotEmpty()) {
-                                throw LanguageAdapterException("Ambiguous $value - refers to both field and method!")
-                            }
-
-                            if (!type.kotlin.isSuperclassOf(fType.jvmErasure)) {
-                                throw LanguageAdapterException("Field " + value + " cannot be cast to " + type.name + "!")
-                            }
-
-                            return field.get(instance) as T
-                        } catch (e: NoSuchFieldException) {
-                            // ignore
-                        } catch (e: IllegalAccessException) {
-                            throw LanguageAdapterException("Field $value cannot be accessed!", e)
-                        }
-                    }
-
-                    if (!type.isInterface) {
-                        throw LanguageAdapterException("Cannot proxy method " + value + " to non-interface type " + type.name + "!")
-                    }
-
-                    if (methodList.isEmpty()) {
-                        throw LanguageAdapterException("Could not find $value!")
-                    } else if (methodList.size >= 2) {
-                        throw LanguageAdapterException("Found multiple method entries of name $value!")
-                    }
-
-                    return Proxy.newProxyInstance(
-                        FabricLauncherBase.getLauncher().targetClassLoader, arrayOf<Class<*>>(type)
-                    ) { proxy, method, args ->
-                        val targetMethod = methodList[0]
-                        targetMethod.call(instance)
-                    } as T
-                } catch(e: UnsupportedOperationException) {
-                    // TODO: detect facades without relying on exceptions
-                    return DefaultLanguageAdapter.INSTANCE.create(mod, value, type)
+                val instance = k.objectInstance ?: run {
+                    // In Kotlin 1.9.20 objectInstance can return null.
+                    // See: https://github.com/FabricMC/fabric-language-kotlin/issues/120
+                    return LanguageAdapter.getDefault().create(mod, value, type)
                 }
+
+                val methodList = instance::class.memberFunctions.filter { m ->
+                    m.name == methodSplit[1]
+                }
+
+                k.declaredMemberProperties.find {
+                    it.name == methodSplit[1]
+                }?.let { field ->
+                    try {
+                        val fType = field.returnType
+
+                        if (methodList.isNotEmpty()) {
+                            throw LanguageAdapterException("Ambiguous $value - refers to both field and method!")
+                        }
+
+                        if (!type.kotlin.isSuperclassOf(fType.jvmErasure)) {
+                            throw LanguageAdapterException("Field " + value + " cannot be cast to " + type.name + "!")
+                        }
+
+                        return field.get(instance) as T
+                    } catch (e: NoSuchFieldException) {
+                        // ignore
+                    } catch (e: IllegalAccessException) {
+                        throw LanguageAdapterException("Field $value cannot be accessed!", e)
+                    }
+                }
+
+                if (!type.isInterface) {
+                    throw LanguageAdapterException("Cannot proxy method " + value + " to non-interface type " + type.name + "!")
+                }
+
+                if (methodList.isEmpty()) {
+                    throw LanguageAdapterException("Could not find $value!")
+                } else if (methodList.size >= 2) {
+                    throw LanguageAdapterException("Found multiple method entries of name $value!")
+                }
+
+                return Proxy.newProxyInstance(
+                    targetClassLoader, arrayOf<Class<*>>(type)
+                ) { proxy, method, args ->
+                    val targetMethod = methodList[0]
+                    targetMethod.call(instance)
+                } as T
             }
             else -> throw LanguageAdapterException("Invalid handle format: $value")
         }
